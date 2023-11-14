36668730877bfd2d61baa3df3766bc387659e957
Landon Bouma <landonb@users.noreply.github.com>
2023-11-14 01:30:20 -0600

    Tests: Bugfix: Remove leftover fixture
    - From the first commit, aed43fb, the EAPP clone-from (back when
      managing boilerplate was done by hand, oh, those were the days).

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,19 +5,3 @@
 # Copyright (c) © 2018-2023 Landon Bouma. All Rights Reserved.
 
 """Test fixtures (none) for the ``easy-as-pypi-getver`` package tests."""
-
-import pytest
-from click.testing import CliRunner
-
-import easy_as_pypi_getver
-
-
-@pytest.fixture
-def runner():
-    """Provide a convenient fixture to simulate execution of (sub-) commands."""
-
-    def runner(args=[], **kwargs):
-        env = {}
-        return CliRunner().invoke(easy_as_pypi_getver.cli, args, env=env, **kwargs)
-
-    return runner
